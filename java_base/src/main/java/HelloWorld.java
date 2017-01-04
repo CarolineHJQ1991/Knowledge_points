4523906f3d1e6524402ac277da6dc36cc1893783
caroline.hjq@gmail.com <hanjq@kaolafm.com>
2017-01-04 15:28:00 +0800

    add "hello_world"  in the java_base

---
+++ java_base/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * User: Caroline.Han
+ * Date: 2017-01-03
+ * Time: 下午5:11
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
