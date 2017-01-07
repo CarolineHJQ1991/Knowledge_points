7cd4ebdf912f14a66e9f680e5a1f1befe2b97666
caroline.hjq@gmail.com <hanjq@kaolafm.com>
2017-01-07 17:56:24 +0800

    add declare

---
+++ java_base/src/main/java/declare.java
@@ -0,0 +1,13 @@
+/**
+ * User: Caroline.Han
+ * Date: 2017-01-07
+ * Time: 下午4:12
+ */
+public class declare {
+    public static void main(String[] args) {
+        System.out.println("Declare in the middle");
+        int a;
+        a = 5;
+        System.out.println(a);
+    }
+}
